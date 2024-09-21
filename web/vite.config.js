c29fe945385a21f93cf3b50931c3eebb0c5101bf
Quang Pham <quangpham.245@proton.me>
2024-09-22 01:28:00 +0700

    web: Add vite-plugin-node-polyfills

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
+  plugins: [react(), nodePolyfills(), tsconfigPaths()],
 })
