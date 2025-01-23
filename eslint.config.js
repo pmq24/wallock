a3f9a659266e87a11bbb373e8af06c97e500fa22
Quang Pham <quangpham.245@proton.me>
2025-01-23 21:37:23 +0700

    Set up ESLint

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import globals from 'globals'
+import neostandard from 'neostandard'
+import vue from 'eslint-plugin-vue'
+
+export default [
+  ...neostandard({
+    files: ['./**/*.vue'],
+    filesTs: ['./**/*.vue'],
+    ts: true,
+  }),
+  ...vue.configs['flat/recommended'],
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: globals.browser,
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+      sourceType: 'module',
+    },
+  },
+]
