c5c678ad717776efa9ed64179a8966f4b96b807b
Quang Pham <quangpham.245@proton.me>
2025-01-23 22:09:51 +0700

    Install Vue Router

---
+++ src/router.ts
@@ -0,0 +1,12 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import HealthView from "./views/HealthView.vue";
+
+const routes = [{ path: "/health", component: HealthView }];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
---
+++ src/views/HealthView.vue
@@ -0,0 +1,8 @@
+<template>
+	<header>
+		<h1>Health</h1>
+	</header>
+	<main>
+		Up and running!
+	</main>
+</template>
