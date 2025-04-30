99ec4bd32c83f7bd2b8b8a3046e53e4e279ccef7
Quang Pham <quangpham.245@proton.me>
2025-05-01 00:27:21 +0700

    SettingService: implement create

---
+++ src/models/common.ts
@@ -0,0 +1,15 @@
+export function createStandardSuccess<T extends unknown>(data: T) {
+  return {
+    success: true,
+    data,
+    errors: undefined,
+  };
+}
+
+export function createStandardError<T extends unknown>(errors: T) {
+  return {
+    success: false,
+    data: undefined,
+    errors,
+  };
+}
---
+++ src/models/settings/SettingService.ts
@@ -0,0 +1,39 @@
+import type { AppDexie } from "models/dexie";
+import Setting from "./Setting";
+import * as v from "valibot";
+import { createStandardError, createStandardSuccess } from "models/common";
+import { nanoid } from "nanoid";
+
+class SettingService {
+  constructor(private dexie: AppDexie) {}
+
+  async create(data: SettingService.CreateData) {
+    const validation = v.safeParse(this.getSchema(), data);
+
+    if (!validation.success) {
+      const errors = v.flatten(validation.issues);
+      return createStandardError(errors);
+    }
+
+    const id = await this.dexie.settings.add({
+      id: nanoid(),
+      timeZone: validation.output.timeZone as Setting.TimeZone,
+    });
+
+    return createStandardSuccess(id);
+  }
+
+  private getSchema() {
+    return v.object({
+      timeZone: v.pipe(v.string(), v.values(Setting.TIME_ZONES)),
+    });
+  }
+}
+
+namespace SettingService {
+  export type CreateData = {
+    timeZone: Setting.TimeZone;
+  };
+}
+
+export default SettingService;
