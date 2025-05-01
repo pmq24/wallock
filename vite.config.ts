9faaec91f5050094982ad5423b12752f0a82c7f3
Quang Pham <quangpham.245@proton.me>
2025-05-01 15:59:36 +0700

    TSConfig: config vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [tailwindcss(), vue(), tsconfigPaths({ loose: true })],
   server: {
     port: 3100,
   },
