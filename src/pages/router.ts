4b0026b33ea3da528f00777667cbd847509f7a45
Aiden Pham <quangpham.245@proton.me>
2025-05-05 13:10:50 +0700

    Fix the default wallet nav guard

--- src/pages/router.ts
+++ src/pages/router.ts
@@ -1,58 +1,58 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import CategoriesNew from './categories.new/WlPage.vue'
-import DefaultWallet from './defaultWallet/WlPage.vue'
-import GettingStarted from './gettingStarted/WlPage.vue'
-import { api } from 'providers/api'
+import { createRouter, createWebHistory } from "vue-router";
+import CategoriesNew from "./categories.new/WlPage.vue";
+import DefaultWallet from "./defaultWallet/WlPage.vue";
+import GettingStarted from "./gettingStarted/WlPage.vue";
+import { api } from "providers/api";
 
 const routes = [
   {
-    path: '/categories/new',
-    name: 'categoriesNew',
+    path: "/categories/new",
+    name: "categoriesNew",
     component: CategoriesNew,
   },
   {
-    path: '/default-wallet',
-    name: 'defaultWallet',
+    path: "/default-wallet",
+    name: "defaultWallet",
     component: DefaultWallet,
   },
   {
-    path: '/getting-started',
-    name: 'gettingStarted',
+    path: "/getting-started",
+    name: "gettingStarted",
     component: GettingStarted,
   },
-]
+];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-})
+});
 
 router.beforeEach(async (to) => {
   if (await api.settings.isCreated()) {
-    return true
+    return true;
   }
 
-  if (to.name === 'gettingStarted') {
-    return true
+  if (to.name === "gettingStarted") {
+    return true;
   }
 
-  return { name: 'gettingStarted' }
-})
+  return { name: "gettingStarted" };
+});
 
 router.beforeEach(async (to, from) => {
   if (await api.wallets.count()) {
-    return true
+    return true;
   }
 
-  if (to.name === 'defaultWallet') {
-    return true
+  if (to.name === "defaultWallet") {
+    return true;
   }
 
-  if (from.name !== 'gettingStarted') {
-    return true
+  if (from.name === "gettingStarted") {
+    return true;
   }
 
-  return { name: 'defaultWallet' }
-})
+  return { name: "defaultWallet" };
+});
 
-export default router
+export default router;
