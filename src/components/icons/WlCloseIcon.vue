3f6c48397a9028161c00d9fc0a84175961ecd449
Aiden Pham <quangpham.245@proton.me>
2025-05-16 12:19:59 +0700

    Icons: update color

--- src/components/icons/WlCloseIcon.vue
+++ src/components/icons/WlCloseIcon.vue
@@ -4,6 +4,6 @@
     height="24px"
     viewBox="0 -960 960 960"
     width="24px"
-    fill="#e3e3e3"
+    fill="var(--color-base-content)"
   ><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" /></svg>
 </template>
