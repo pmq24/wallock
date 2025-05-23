2feea67b72df43d39d7b62e9f622c11c1f8c95b9
Aiden Pham <quangpham.245@proton.me>
2025-05-23 12:25:02 +0700

    web.sync: change Hasher's hash algorithm from SHA256 to SHA1

--- web/src/models/sync/hashes/Hasher.ts
+++ web/src/models/sync/hashes/Hasher.ts
@@ -1,12 +1,12 @@
 import bencode from 'bencode'
 import { Base64 } from 'js-base64'
-import jsSha256 from 'js-sha256'
+import { sha1 } from 'js-sha1'
 
 export default class Hasher {
   hashData (data: Omit<Record<string, any>, 'hash'>) {
     const bencoded = bencode.encode(data)
 
-    const sha256 = jsSha256.sha256(bencoded)
+    const sha256 = sha1(bencoded)
     return Base64.fromUint8Array(
       new Uint8Array(this.textEncoder.encode(sha256))
     )
@@ -17,7 +17,7 @@ export default class Hasher {
       .map((item) => item.hash)
       .sort()
       .join(' ')
-    const sha256 = jsSha256.sha256(joined)
+    const sha256 = sha1(joined)
 
     return Base64.fromUint8Array(
       new Uint8Array(this.textEncoder.encode(sha256))
