073ecf404ca5e03750eec351dc05526bbe4dd6bc
Aiden Pham <quangpham.245@proton.me>
2025-06-24 23:06:09 +0700

    web: add explanation for vite-env.d.ts

--- web/src/vite-env.d.ts
+++ web/src/vite-env.d.ts
@@ -1,1 +1,17 @@
+// Declaring types for `import.meta.env`
+// https://vite.dev/guide/env-and-mode#intellisense-for-typescript
+
 /// <reference types="vite/client" />
+
+interface ViteTypeOptions {
+  // By adding this line, you can make the type of ImportMetaEnv strict
+  // to disallow unknown keys.
+  strictImportMetaEnv: unknown
+}
+
+interface ImportMetaEnv {
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
