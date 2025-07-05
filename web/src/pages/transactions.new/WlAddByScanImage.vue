c74744e8ae3daabd2a7f73445ac035a22f8e902a
Aiden Pham <quangpham.245@proton.me>
2025-07-06 00:26:13 +0700

    web:transactions.new: add image-scanning tab

---
+++ web/src/pages/transactions.new/WlAddByScanImage.vue
@@ -0,0 +1,17 @@
+<template>
+  <form class="flex flex-col gap-4 p-2 items-center mt-8">
+    <hgroup class="flex flex-col items-center">
+      <WlCameraIconFilled class="size-30 fill-base-200" />
+      <p>Quickly create a new transaction from images like bills, bank receipts, ...</p>
+    </hgroup>
+
+    <input
+      type="file"
+      class="file-input"
+    >
+  </form>
+</template>
+
+<script setup lang="ts">
+import { WlCameraIconFilled } from 'components/icons'
+</script>
