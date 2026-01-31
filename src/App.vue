0fe969778ead1977314b517afa3490a03c8ff44d
Aiden Pham <quangpham.245@proton.me>
2026-01-31 23:45:08 +0700

    Init `web` project
    Vue, TailwindCSS, and DaisyUI

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>You did it!</h1>
+  <p class="bg-red-100">
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+
+  <button class="btn btn-primary">Tailwind CSS Button</button>
+</template>
+
+<style scoped></style>
