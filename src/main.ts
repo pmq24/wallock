a7e6e1ec93f12a52f98d32190dbd6f1468abebd0
Quang Pham (Aiden) <quangpham.245@proton.me>
2026-02-12 18:43:49 +0700

    Install Vue I18n

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,23 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { createI18n } from 'vue-i18n'
+import en from '@/i18n/en'
+
 import * as ApiProvider from '@/api-provider'
 
 const app = createApp(App)
 
 app.use(router)
+
+const i18n = createI18n({
+  locale: 'en',
+  fallbackLocale: 'en',
+  messages: {
+    en
+  }
+})
+app.use(i18n)
 
 app.provide(ApiProvider.KEY, ApiProvider.singleton)
 
