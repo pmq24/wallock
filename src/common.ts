b573bad136e4142361da8ec7b015a01da2f4b6dd
Quang Pham (Aiden) <quangpham.245@proton.me>
2026-02-25 18:39:40 +0700

    2026.02.25

--- src/common.ts
+++ src/common.ts
@@ -1,5 +1,7 @@
 import { useTranslation } from 'i18next-vue'
 import * as ApiProvider from '@/api-provider'
+import { ref } from 'vue'
+import { useAsyncState } from '@vueuse/core'
 
 export function useCommon() {
   const api = ApiProvider.inject()
@@ -15,6 +17,38 @@ export function useScopedTranslate(scope: string) {
       return t(`${scope}.${key}`, { count })
     } else {
       return t(`${scope}.${key}`)
+    }
+  }
+}
+
+export type Okable<TData, TError> =
+  | Readonly<{ ok: true; data: TData }>
+  | Readonly<{ ok: false; error: TError }>
+
+export function useForm<
+  TData extends Record<string, unknown>,
+  TResult extends unknown,
+  TError extends unknown
+>(opts: {
+  initialValues: TData,
+  validate: (data: TData) => Promise<Okable<TData, TError>>,
+  submit: (data: TData) => Promise<Okable<TResult, TError>>,
+}) {
+  const data = ref(opts.initialValues)
+
+  const {state: submission, execute: submit} = useAsyncState(
+    () => opts.submit(data.value),
+    undefined,
+    {
+      immediate: false,
+    }
+  )
+
+  return {
+    data,
+    submit,
+    extractError(extractor: (error: TError) => string) {
+
     }
   }
 }
